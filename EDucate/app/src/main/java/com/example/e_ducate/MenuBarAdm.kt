6654d0930683cda481e660ab047957e2c10f62fe
Bruno_Sampaio_SSG <bruno.sampaio@tapps.games>
2021-11-16 19:28:28 -0300

    menubar do adm concluido

--- EDucate/app/src/main/java/com/example/e_ducate/MenuBarAdm.kt
+++ EDucate/app/src/main/java/com/example/e_ducate/MenuBarAdm.kt
@@ -1,50 +1,40 @@
 package com.example.e_ducate
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import com.example.e_ducate.databinding.ActivityMenuBarBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.e_ducate.databinding.ActivityMenuBarAdmBinding
+import com.google.android.material.navigation.NavigationView
 
-class MenuBar : AppCompatActivity() {
-
+class MenuBarAdm : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMenuBarBinding
+    private lateinit var binding: ActivityMenuBarAdmBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMenuBarBinding.inflate(layoutInflater)
+        binding = ActivityMenuBarAdmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMenuBar.toolbar)
+        setSupportActionBar(binding.appBarMenuBarAdm.toolbarAdm)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_menu_bar)
+        val navController = findNavController(R.id.nav_host_fragment_content_menu_bar_adm)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home_adm,
-                R.id.nav_profile_adm,
-                R.id.nav_contact_adm,
-                R.id.nav_help_adm,
+//                R.id.nav_profile,
+//                R.id.nav_contact,
+//                R.id.nav_help,
 
             ), drawerLayout
         )
@@ -66,9 +56,5 @@ class MenuBar : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_menu_bar)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    fun irTelaPesquisa(v: View){
-        startActivity(Intent(this, PesquisaBibliotecas::class.java))
     }
 }
